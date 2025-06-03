5d98b6a9aa00a38bd7fe2292be575c53ec47bf5e
achutha02 <bidarahalliachyuta@gmail.com>
2025-06-04 00:46:24 +0530

    Updated file

--- Sorting_technique/practice.cpp
+++ Sorting_technique/practice.cpp
@@ -1,44 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int func(vector<int> &arr,int low,int high){
-	int pivot=arr[low];
-	int i=low;
-	int j=high;
-	while(i<j){
-		while(arr[i]<=pivot && i<=high-1){
-			i++;
+void selection_sort(int arr[],int n){
+	for(int i=0;i<n-1;i++){
+		int mini=i;
+		for(int j=i+1;j<n;j++){
+			if(arr[j]<arr[mini]){
+				mini=j;
+			}
 		}
-		while(arr[j]>=pivot && j>=low+1){
-			j--;
-		}
-		if(i<j){
-			swap(arr[i],arr[j]);
-		}
-	}
-	swap(arr[low],arr[j]);
-	return j;
-}
-void quick_sort(vector<int> &arr,int low,int high){
-	if(low<high){
-		int pIndex=func(arr,low,high);
-		quick_sort(arr,low,pIndex-1);
-		quick_sort(arr,pIndex+1,high);
+		int temp=arr[mini];
+		arr[mini]=arr[i];
+		arr[i]=temp;
 	}
 }
 int main(){
-	vector<int> arr={4,6,2,5,7,9,1,3};
-	int n=arr.size();
-	cout << "Before Quick Sort" << endl;
+	int arr[]={9,46,24,52,20,13};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	selection_sort(arr,n);
 	for(int i=0;i<n;i++){
 		cout << arr[i] << " ";
 	}
-	quick_sort(arr,0,n-1);
-	cout << endl;
-	cout << endl;
-	cout << "After Quick Sort" << endl;
-	for(int i=0;i<n;i++){
-		cout << arr[i] << " ";
-	}
-	
+	return 0;	
 }
--- Sorting_technique/selection_sort1.cpp
+++ Sorting_technique/selection_sort1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void selection_sort(int arr[],int n){
-	for(int i=0;i<=n-2;i++){
+	for(int i=0;i<n-1;i++){
 		int mini=i;
-		for(int j=i;j<=n-1;j++){
+		for(int j=i+1;j<n;j++){
 			if(arr[j]<arr[mini]){
 				mini=j;
 			}
@@ -15,12 +15,8 @@ void selection_sort(int arr[],int n){
 	}
 }
 int main(){
-	int n;
-	cin >> n;
-	int arr[n];
-	for(int i=0;i<n;i++){
-		cin >> arr[i];
-	}
+	int arr[]={9,46,24,52,20,13};
+	int n=sizeof(arr)/sizeof(arr[0]);
 	selection_sort(arr,n);
 	for(int i=0;i<n;i++){
 		cout << arr[i] << " ";
