12b708b5373a4651f855635ae565f73938dadaa0
achutha02 <bidarahalliachyuta@gmail.com>
2025-06-12 05:58:33 +0530

    Added new file

--- Arrays/practice.cpp
+++ Arrays/practice.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 
 int main(){
-	int arr[]={2,3,5,1,9};
-	int n=sizeof(arr)/sizeof(arr[0]);
-	int length=0;
-	int target=10;
-	for(int i=0;i<n;i++){
-		for(int j=i;j<n;j++){
-			int sum=0;
-			for(int k=i;k<j;k++){
-				sum+=arr[k];
-				if(sum==target){
-					int currLength=j-i+1;
-					if(currLength>length){
-						length=currLength;
-					}
-				}
-			}
+	vector<int> a={1,2,3,1,1,1,1,4,2,3};
+	int maxLen=0;
+	int sum=0;
+	int k=3;
+	map<int,int> preSum;
+	for(int i=0;i<a.size();i++){
+		sum+=a[i];
+		if(sum==k){
+			maxLen=max(maxLen,i+1);
+		}
+		int rem=sum-k;
+		if(preSum[rem] != preSum.end()){
+			int len=i-preSum[rem];
+			maxLen=max(maxLen,len);
+		}
+		if(preSum[sum] == preSum.end()){
+			preSum[sum]=i;
 		}
 	}
-	cout << length;
+	cout << maxLen;
 	return 0;
 }
---
+++ Arrays/two_sum_better_app.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int arr[]={2,6,5,8,11};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	unordered_map<int,int> mpp;
+	int target=14;
+	for(int i=0;i<n;i++){
+		int num=arr[i];
+		int moreNeeded=target-num;
+		if(mpp.find(moreNeeded) != mpp.end()){
+			cout << mpp[moreNeeded] << " "<< i << endl;
+		}
+		mpp[num]=i;
+	}
+	return 0;
+}
