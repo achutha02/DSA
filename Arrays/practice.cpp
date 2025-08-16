d7d27c998a8bc974f71421021cb6410c9068c186
achutha02 <bidarahalliachyuta@gmail.com>
2025-08-16 16:06:40 +0530

    Updated file

--- Arrays/practice.cpp
+++ Arrays/practice.cpp
@@ -1,26 +1,24 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-	vector<int> a={1,2,3,1,1,1,1,4,2,3};
-	int maxLen=0;
-	int sum=0;
-	int k=3;
-	map<int,int> preSum;
-	for(int i=0;i<a.size();i++){
-		sum+=a[i];
-		if(sum==k){
-			maxLen=max(maxLen,i+1);
-		}
-		int rem=sum-k;
-		if(preSum[rem] != preSum.end()){
-			int len=i-preSum[rem];
-			maxLen=max(maxLen,len);
-		}
-		if(preSum[sum] == preSum.end()){
-			preSum[sum]=i;
-		}
+	cout << "Enter the number: ";
+	int n;
+	cin >> n;
+	int dup = n;
+	int size = to_string(n).size();
+	int sum = 0;
+	while(n>0){
+		int lastdigit = n%10;
+		int result = pow(lastdigit,size);
+		sum = sum+result;
+		n=n/10;
 	}
-	cout << maxLen;
+	if(sum==dup){
+		cout << "The number is armstrong number";
+	}
+	else{
+		cout << "The number is not armstrong number";
+	}
 	return 0;
 }
--- Basic_maths/armstrong_number.cpp
+++ Basic_maths/armstrong_number.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main(){
+	cout << "Enter a number: ";
 	int n;
 	cin >> n;
-	int dup=n;
-	int size=sizeof(n);
-	int sum=0;
-	int revNum=0;
+	int dup =n;
+	int size = to_string(n).size();
+	int sum = 0;
 	while(n>0){
-		int lastdigit=n%10;
-		double x=lastdigit, y=size;
-		int result= pow(x,y);
-		sum=sum+result;
-		n=n/10;
+		int lastdigit = n%10;
+		int result = pow(lastdigit,size);
+		sum += result;
+		n /= 10;
 	}
 	if(sum==dup){
 		cout << "The number is armstrong number";
 	}
 	else{
-		cout << "The number is not armstrong";
+		cout << "The number is not armstrong number";
 	}
+	return 0;
 }
