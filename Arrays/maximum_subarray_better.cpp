f727cc32fcae9f45cfe56b200cddd24f5a1367c9
achutha02 <bidarahalliachyuta@gmail.com>
2025-09-28 11:38:14 +0530

    Added new file

--- Arrays/maximum_subarray_better.cpp
+++ Arrays/maximum_subarray_better.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 	int arr[]={-2,1,-3,4,-1,2,1,-5,4};
 	int n=sizeof(arr)/sizeof(arr[0]);
-	int max_sum=INT_MIN;
+	int max_sum=0;
 	for(int i=0;i<n;i++){
 		int sum=0;
 		for(int j=i;j<n;j++){
