db2233c702d2b4a501dd67221703fcac7f27ebff
achutha02 <bidarahalliachyuta@gmail.com>
2025-10-30 13:29:39 +0530

    Added new file

--- Arrays/Find_Union_optimal.cpp
+++ Arrays/Find_Union_optimal.cpp
@@ -2,35 +2,35 @@
 using namespace std;
 
 int main(){
-	int arr1[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int arr2[]={2, 3, 4, 4, 5, 11, 12};
+	int arr1[]={1,2,3,4,5,6,7,8,9,10};
+	int arr2[]={2,3,4,4,5,11,12};
 	int n=sizeof(arr1)/sizeof(arr1[0]);
 	int m=sizeof(arr2)/sizeof(arr2[0]);
+	vector<int> Union;
 	int i=0;
 	int j=0;
-	vector<int> Union;
 	while(i<n && j<m){
-		if(arr1[i]<=arr2[j]){
-			if(Union.size()==0 || Union.back()!=arr1[i]){
+		if(arr1[i] <= arr2[j]){
+			if(Union.size()==0 || Union.back() != arr1[i]){
 				Union.push_back(arr1[i]);
 			}
 			i++;
 		}
 		else{
-			if(Union.size()==0 || Union.back()!=arr2[j]){
+			if(Union.size()==0 || Union.back() != arr2[j]){
 				Union.push_back(arr2[j]);
 			}
 			j++;
 		}
 	}
 	while(i<n){
-		if(Union.back()!=arr1[i]){
+		if(Union.back() != arr1[i]){
 			Union.push_back(arr1[i]);
 			i++;
 		}
 	}
 	while(j<m){
-		if(Union.back()!=arr2[j]){
+		if(Union.back() != arr2[j]){
 			Union.push_back(arr2[j]);
 			j++;
 		}
