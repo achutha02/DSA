fc3a02505120cf90430df68a6b5a73c1b08a5bfd
achutha02 <bidarahalliachyuta@gmail.com>
2025-11-25 12:13:00 +0530

    Added new file

--- Arrays/Longest_subarray_with_given_sum_K_brute.cpp
+++ Arrays/Longest_subarray_with_given_sum_K_brute.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main(){
-	int arr[] = {10, 5, 2, 7, 1, 9};
+	int arr[] = {1,2,3,1,1,1,1,4,2,3};
 	int n=sizeof(arr)/sizeof(arr[0]);
+	int target=3;
 	int max_count=0;
-	int target=15;
 	for(int i=0;i<n;i++){
 		int count=0;
 		int sum=0;
